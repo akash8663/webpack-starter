36da43f93812c1967588b80280c5841a567def16
Abrar.Akash <[abrar.akash@adp.com]>
2019-08-22 21:30:57 -0400

    First commit of my webpack started project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,33 @@
-import '../styles/index.scss';
+console.log('Tell me, Do you bleed!!!!!');
+const id = 100;
+console.log(id);
+var varid = 10;
+console.log(varid);
+var letid = 10;
+console.log(letid);
 
-console.log('webpack starterkit');
+function restParam(car,...cars){        //rest param
+    // console.log(car);
+    cars.forEach(id=> console.log(id));
+    // console.log(car2);
+    // console.log(car3);
+}
+
+let cars = "cars";
+
+restParam("car1","car2", 100);
+
+let moons = [1,2,3,4,5];        //destructuring arrays
+// let moon1, moon;
+let [,moon1,...moon] = moons;
+console.log(moons);
+
+let states = {name: "CA", loc:"west"};     //destructuring objects
+let {name, loc} = states;
+console.log(states);
+
+let s1 = "Zoo";
+let s2 = "alphabets";
+
+console.log(s1.toUpperCase < s2.toUpperCase? true:false);
+
